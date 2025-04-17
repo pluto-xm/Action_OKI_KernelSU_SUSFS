1d7da49bd589fa77b8e987f9d2f6191a441714dc
pluto-xm <107329779+pluto-xm@users.noreply.github.com>
2025-04-17 09:53:41 +0800

    更新 build_mksu_sm8650.sh

--- build_shell/build_mksu_sm8650.sh
+++ build_shell/build_mksu_sm8650.sh
@@ -49,7 +49,7 @@ find . -type d > "$OLD_DIR/kernel_directory_structure.txt"
 
 # 设置 KernelSU
 cd "$KERNEL_WORKSPACE" || exit 1
-curl -LSs "https://raw.githubusercontent.com/5ec1cff/KernelSU/refs/heads/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/pluto-xm/SukiSU-Ultra/refs/heads/main/kernel/setup.sh" | bash -s main
 cd KernelSU || exit 1
 git revert -m 1 "$(git log --grep="remove devpts hook" --pretty=format:"%H")" -n
 KSU_VERSION=$(expr "$(git rev-list --count HEAD)" + 10200)
